9a7af92b8e6c224276232141f9ae8defbec35955
Wladyslaw <kvazid@gmail.com>
2021-05-28 20:19:00 +0700

    сверстал карточку персонажа + главную страницу

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,12 @@ import styles from './Header.module.css';
 
 const Header = () => {
     return (
-        <div className={styles.header}>
+        <header className={styles.header}>
             <ul className={styles.menu}>
                 <li><a href="#" className={styles.menuLink}>Главная</a></li>
                 <li><a href="#" className={styles.menuLink}>Любимые герои</a></li>
             </ul>
-        </div>
+        </header>
     )
 }
 
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,99 @@
+import styles from './MainPage.module.css';
+
 const MainPage = () => {
     return (
-        <p>Clean application</p>
+        <div className={styles.character_list}>
+            <div className={styles.character_card}>
+                <div>
+                    <img className={styles.character_img} src="https://starwars-visualguide.com/assets/img/characters/1.jpg" />
+                </div>
+                <div className={styles.character_info}>
+                    <div>
+                        <span>Luke Skywalker</span>
+                    </div>
+                    <div>
+                        <button>
+                            ❤
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.character_card}>
+                <div>
+                    <img className={styles.character_img} src="https://starwars-visualguide.com/assets/img/characters/1.jpg" />
+                </div>
+                <div className={styles.character_info}>
+                    <div>
+                        <span>Luke Skywalker</span>
+                    </div>
+                    <div>
+                        <button>
+                            ❤
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.character_card}>
+                <div>
+                    <img className={styles.character_img} src="https://starwars-visualguide.com/assets/img/characters/1.jpg" />
+                </div>
+                <div className={styles.character_info}>
+                    <div>
+                        <span>Luke Skywalker</span>
+                    </div>
+                    <div>
+                        <button>
+                            ❤
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.character_card}>
+                <div>
+                    <img className={styles.character_img} src="https://starwars-visualguide.com/assets/img/characters/1.jpg" />
+                </div>
+                <div className={styles.character_info}>
+                    <div>
+                        <span>Luke Skywalker</span>
+                    </div>
+                    <div>
+                        <button>
+                            ❤
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.character_card}>
+                <div>
+                    <img className={styles.character_img} src="https://starwars-visualguide.com/assets/img/characters/1.jpg" />
+                </div>
+                <div className={styles.character_info}>
+                    <div>
+                        <span>Luke Skywalker</span>
+                    </div>
+                    <div>
+                        <button>
+                            ❤
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.character_card}>
+                <div>
+                    <img className={styles.character_img} src="https://starwars-visualguide.com/assets/img/characters/1.jpg" />
+                </div>
+                <div className={styles.character_info}>
+                    <div>
+                        <span>Luke Skywalker</span>
+                    </div>
+                    <div>
+                        <button>
+                            ❤
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
