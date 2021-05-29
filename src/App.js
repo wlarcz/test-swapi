fdaf7ddc01e4b62aff007f0e5a301ca70f082b80
Wladyslaw <kvazid@gmail.com>
2021-05-29 22:19:39 +0700

    переписал ссылки в шапке на NavLink

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import FavoriteCharacters from './components/FavoriteCharacters/FavoriteCharacters';
 import Header from './components/Header/Header'
@@ -5,13 +6,16 @@ import MainPage from './components/MainPage/MainPage';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <div className="content-wrapper">
-        <MainPage />
-        <FavoriteCharacters />
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className="content-wrapper">
+          {/* <Route path='/main' component={MainPage} /> */}
+          <Route exact path='/' component={MainPage} />
+          <Route exact path='/favorites' component={FavoriteCharacters} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <ul className={styles.menu}>
-                <li><a href="#" className={styles.menuLink}>Главная</a></li>
-                <li><a href="#" className={styles.menuLink}>Любимые герои</a></li>
+                {/* <li><a href="/main" className={styles.menuLink}>Главная</a></li> */}
+                <li><NavLink exact to="/" className={styles.menuLink} activeClassName={styles.activeLink}>Главная</NavLink></li>
+                <li><NavLink to="/favorites" className={styles.menuLink} activeClassName={styles.activeLink}>Любимые герои</NavLink></li> 
             </ul>
         </header>
     )
